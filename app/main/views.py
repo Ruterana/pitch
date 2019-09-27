1f4caa02b0b9e50d29460a5ab780f5582ab405d1
Ruterana <ncharlene38@gmail.com>
2019-09-27 15:38:10 +0200

    imake changes in views.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,39 +1,39 @@
-# from flask import render_template,request,redirect,url_for,abort
-# from ..models import User,Pitch
-# from . import main
-# from .forms import PitchForm
-# from .. import db,photos
-# from flask_login import login_required,current_user
-# import markdown2 
+from flask import render_template,request,redirect,url_for,abort
+from ..models import User,Pitch
+from . import main
+from .forms import PitchForm
+from .. import db,photos
+from flask_login import login_required,current_user
+import markdown2 
 
-# @main.route('/', methods = ['GET','POST'])
-# def index():
-#     '''
-#     View root page function that returns the index page and its data
-#     '''
+@main.route('/', methods = ['GET','POST'])
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
     
-#     pitch = Pitch.query.all()
-#     title = 'Welcome Home-Minute Pitch'
-#     love = Pitch.query.filter_by(category="love")
-#     product = Pitch.query.filter_by(category = "product")
-#     motivation = Pitch.query.filter_by(category = "motivation")
+    pitch = Pitch.query.all()
+    title = 'Welcome Home-Minute Pitch'
+    love = Pitch.query.filter_by(category="love")
+    product = Pitch.query.filter_by(category = "product")
+    motivation = Pitch.query.filter_by(category = "motivation")
     
-#     return render_template('index.html', title = title, pitch = pitch, love=love, product= product, motivation = motivation)
+    return render_template('index.html', title = title, pitch = pitch, love=love, product= product, motivation = motivation)
 
 
 
 
-#     return render_template('index.html',title = title)
-# @main.route('/pitch/new',methods=['GET','POST'])
-# @login_required
-# def new_pitch():
-#     form= PitchForm()
-#     if form.validate_on_submit():
-#         title = form.title.data
-#         description = form.description.data
-#         category= form.category.data
-#         new_pitch = Pitch(user_id =current_user._get_current_object().id, title = title,description=description,category=category)
-#         db.session.add(new_pitch)
-#         db.session.commit()
-#         return redirect (url_for('main.index'))
-#     return render_template('pitch.html',pitch_form=form)
+    return render_template('index.html',title = title)
+@main.route('/pitch/new',methods=['GET','POST'])
+@login_required
+def new_pitch():
+    form= PitchForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        description = form.description.data
+        category= form.category.data
+        new_pitch = Pitch(user_id =current_user._get_current_object().id, title = title,description=description,category=category)
+        db.session.add(new_pitch)
+        db.session.commit()
+        return redirect (url_for('main.index'))
+    return render_template('pitch.html',pitch_form=form)
