8de95ab9c64c3ace1300497ec80d983d0c1fb7cb
Ruterana <ncharlene38@gmail.com>
2019-09-27 15:35:05 +0200

    imake changes in forms.py

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,13 +1,13 @@
-#
-from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField,TextAreaField,RadioField
-from wtforms.validators import Required,Email,EqualTo
-from wtforms import ValidationError
+# #
+# from flask_wtf import FlaskForm
+# from wtforms import StringField,SubmitField,TextAreaField,RadioField
+# from wtforms.validators import Required,Email,EqualTo
+# from wtforms import ValidationError
 
 
 
-class PitchForm(FlaskForm):
-	title = StringField('Title', validators=[Required()])
-	description = TextAreaField("write your pitch here?",validators=[Required()])
-	category = RadioField('Label', choices =[ ('love','love'),('product','product'),('motivation','motivation')],validators=[Required()])
-	submit = SubmitField('Submit')
+# class PitchForm(FlaskForm):
+# 	title = StringField('Title', validators=[Required()])
+# 	description = TextAreaField("write your pitch here?",validators=[Required()])
+# 	category = RadioField('Label', choices =[ ('love','love'),('product','product'),('motivation','motivation')],validators=[Required()])
+# 	submit = SubmitField('Submit')
